7eb965af0eea9e4b4a1ba984ea1353f1eb0d881e
veerabala <imveerabala@gmail.com>
2024-09-02 12:15:56 +0530

    first commit

---
+++ kg-app-frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './app.scss'
+import Home from './pages/home/Home'
+
+function App() {
+
+  return (
+    <>
+     <Home />
+    </>
+  )
+}
+
+export default App
---
+++ kg-app-frontend/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import './home.scss';
+
+function Home() {
+    return (
+        <div className='heading'>
+            KG-APP-Frontend with Vite and SCSS
+            <p className='heading-text'>
+                Hello world!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
